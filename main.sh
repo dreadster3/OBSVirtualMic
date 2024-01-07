7c9c21017022ec4291d0b554a80657cff9fc9f91
dreadster3 <afonso.antunes@live.com.pt>
2024-01-08 00:16:10 +0400

    Fix outputs and add logs

--- main.sh
+++ main.sh
@@ -4,22 +4,27 @@ mic_name="OBSMic"
 combined_sink_name="OBSCombined"
 
 # Unload module
-pactl unload-module module-null-sink || true
-pactl unload-module module-combine-sink || true
+echo "Unloading modules"
+pactl unload-module module-null-sink 2> /dev/null || true
+pactl unload-module module-combine-sink 2> /dev/null || true
 
 # Create virtual sink
-pactl load-module module-null-sink media.class=Audio/Sink sink_name=$mic_name channel_map=front-left,front-right
+pactl load-module module-null-sink media.class=Audio/Sink sink_name=$mic_name channel_map=front-left,front-right > /dev/null
+echo "Succesfully created virtual sink: $mic_name"
 
 # Create virtual mic
-pactl load-module module-null-sink media.class=Audio/Source/Virtual sink_name=$mic_name channel_map=front-left,front-right
+pactl load-module module-null-sink media.class=Audio/Source/Virtual sink_name=$mic_name channel_map=front-left,front-right > /dev/null
+echo "Succesfully created virtual source: $mic_name"
 
 # Wait for last command to take effect
 sleep 1
 
 # Link mic to sink
-pw-link "$mic_name:monitor_FL" "$mic_name:input_FL"
-pw-link "$mic_name:monitor_FR" "$mic_name:input_FR"
+pw-link $mic_name:monitor_FL $mic_name:input_FL
+pw-link $mic_name:monitor_FR $mic_name:input_FR
+echo "Successfully linked sink to source"
 
 # Create a combined sink
 default_sink=$(pactl get-default-sink)
-pactl load-module module-combine-sink sink_name=$combined_sink_name slaves=$mic_name,$default_sink
+echo "Combining default sink ($default_sink) with $mic_name"
+pactl load-module module-combine-sink sink_name="$combined_sink_name" slaves="$mic_name,$default_sink" > /dev/null
